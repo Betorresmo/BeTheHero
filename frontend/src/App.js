2c432a121dcf8d69566c1babfdbc58182537550d
Roberto <rleisguerrero@gmail.com>
2020-03-24 19:55:46 -0300

    database add and query ongs and incidents

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Roberto <code>codigoroboto</code>
+          Hello Beto.
         </p>
         <a
           className="App-link"
-          href="https://www.google.com"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Beto
+          Learn React
         </a>
       </header>
     </div>

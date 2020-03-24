9ee38b1708b2945855705508bbee867763de3418
Roberto <rleisguerrero@gmail.com>
2020-03-24 20:11:17 -0300

    delete incident

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,7 +3,6 @@ const connection = require('../database/connection');
 module.exports = {
 
     async index(request, response){
-        
         const incidents = await connection('incidents').select('*');
 
         return response.json(incidents);
@@ -20,5 +19,22 @@ module.exports = {
         });
 
         return response.json({ id });
+    },
+    async delete(request, response){
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
+
+        const incident = await connection('incidents')
+        .where('id', id)
+        .select('ong_id')
+        .first();
+
+        if(incident.ong_id != ong_id){
+            return response.status(401).json({error: 'Operation not authorized.'})
+        }
+
+        await connection('incidents').where('id', id).delete();
+
+        return response.status(204).send();
     }
 };

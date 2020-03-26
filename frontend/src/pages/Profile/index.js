72dd8ef030913c9d424779a18d4189593cdd40e2
Roberto <rleisguerrero@gmail.com>
2020-03-25 21:03:30 -0300

    Logout

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,15 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import logoImg from '../../assets/logo.svg';
-import { Link} from 'react-router-dom';
+import { Link, useHistory} from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import api from '../../services/api'
 import './style.css';
 
 function Profile (){
 
+    const history = useHistory();
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
-
     const [incidents, setIncidents] = useState([]);
 
     useEffect(() => {
@@ -38,6 +38,11 @@ function Profile (){
         }
     }
 
+    function handleLogout(){
+        localStorage.clear();
+        history.push('/');
+    }
+
     return(
         <div className="profile-container">
             <header>
@@ -45,7 +50,7 @@ function Profile (){
                 <span>Bem vinda, {ongName}</span>
                 <Link className="button" to="/incidents/new">Cadastrar um novo caso</Link>
                 <button type="button">
-                    <FiPower size={18} color="#E02041" />
+                    <FiPower onClick={handleLogout} size={18} color="#E02041" />
                 </button>
             </header>
             <h1>Casos cadastrados</h1>
